b6e95b9750b91c0f4bdd9df98e14bf9bcb9a9f0d
Joseph Mingrone <jrm@ftfl.ca>
2016-03-04 14:43:57 -0400

    Comment unused extensions and uBlock stuff

--- conkeror/003-extensions.js
+++ conkeror/003-extensions.js
@@ -1,44 +1,52 @@
 // cookie culler
-const cookie_culler_chrome = "chrome://cookieculler/content/CookieCuller.xul";
+// const cookie_culler_chrome = "chrome://cookieculler/content/CookieCuller.xul";
 
-interactive("cookie-culler-dialog", "Show the CookieCuller settings in a dialog box.",
-	    function (I) {
-		var frame = I.buffer.top_frame;
-		frame.openDialog(cookie_culler_chrome,
-				 "CookieCuller",
-				 "centerscreen,chrome,dialog,modal,resizable");
-	    });
+// interactive("cookie-culler-dialog", "Show the CookieCuller settings in a dialog box.",
+// 	    function (I) {
+// 		var frame = I.buffer.top_frame;
+// 		frame.openDialog(cookie_culler_chrome,
+// 				 "CookieCuller",
+// 				 "centerscreen,chrome,dialog,modal,resizable");
+// 	    });
 
-interactive("cookie-culler", "Open the CookieCuller settings in a new buffer.",
-	    "find-url-new-buffer",
-	    $browser_object = cookie_culler_chrome);
+// interactive("cookie-culler", "Open the CookieCuller settings in a new buffer.",
+// 	    "find-url-new-buffer",
+// 	    $browser_object = cookie_culler_chrome);
 
 // firebug
-define_variable("firebug_url",
-    "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js");
+// define_variable("firebug_url",
+//     "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js");
 
-function firebug (I) {
-    var doc = I.buffer.document;
-    var script = doc.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', firebug_url);
-    script.setAttribute('onload', 'firebug.init();');
-    doc.body.appendChild(script);
-}
-interactive("firebug", "open firebug lite", firebug);
+// function firebug (I) {
+//     var doc = I.buffer.document;
+//     var script = doc.createElement('script');
+//     script.setAttribute('type', 'text/javascript');
+//     script.setAttribute('src', firebug_url);
+//     script.setAttribute('onload', 'firebug.init();');
+//     doc.body.appendChild(script);
+// }
+// interactive("firebug", "open firebug lite", firebug);
 
 // mozrepl
-user_pref('extensions.mozrepl.autoStart', true);
+// user_pref('extensions.mozrepl.autoStart', true);
 
-let (mozrepl_init = get_home_directory()) {
-    mozrepl_init.appendRelativePath(".mozrepl-conkeror.js");
-    session_pref('extensions.mozrepl.initUrl', make_uri(mozrepl_init).spec);
-}
+// let (mozrepl_init = get_home_directory()) {
+//     mozrepl_init.appendRelativePath(".mozrepl-conkeror.js");
+//     session_pref('extensions.mozrepl.initUrl', make_uri(mozrepl_init).spec);
+// }
 
-// uBlock (via ebzzry)
 interactive(
     "ublock", "Open uBlock dashboard in a new buffer",
     function (I) {
-        load_url_in_new_buffer("chrome://ublock/content/dashboard.html");
+        var ublock_branch;
+        if ("@ublock0/content-policy;1" in Cc) {
+            ublock_branch = "ublock0";
+        } else if ("@ublock/content-policy;1" in Cc) {
+            ublock_branch = "ublock";
+        } else {
+            throw interactive_error("uBlock not found");
+        }
+        load_url_in_new_buffer("chrome://"+ublock_branch+"/content/dashboard.html");
     }
 );
+
