14ea59ca48916c02047a648dbc25e24bac466647
Joseph Mingrone <jrm@ftfl.ca>
2017-08-29 12:04:58 -0300

    pdf-view-mode: fit page; do not change pages when reaching page edge

--- emacs/custom.el
+++ emacs/custom.el
@@ -482,6 +482,8 @@
  '(package-gnupghome-dir "~/.gnupg")
  '(package-selected-packages
    '(calfw-cal calfw-org calfw org ebib dired+ which-key key-chord flycheck smart-mode-line company-quickhelp flycheck-package company-math slime-company slime pdf-tools goto-last-change git-gutter-fringe exwm company-statistics sauron company-auctex company-bibtex company-c-headers company-ghc company-ghci scpaste php-mode polymode rebox2 rnc-mode smart-tab stumpwm-mode synosaurus twittering-mode undo-tree visible-mark wttrin yaml-mode znc ivy-bibtex misc-cmds multi-eshell multi-term multi-web-mode names nginx-mode org-bullets paredit htmlize hydra intero ivy-hydra google-translate genrnc ghc ghci-completion google-maps google-this magit ace-link ace-popup-menu aggressive-fill-paragraph auctex-latexmk avy-zap bbdb boxquote buffer-move conkeror-minor-mode counsel define-word erc-view-log es-lib ess esup fill-column-indicator ace-window aggressive-indent beacon bug-hunter exec-path-from-shell))
+ '(pdf-view-continuous nil)
+ '(pdf-view-display-size 'fit-page)
  '(polymode-display-process-buffers nil)
  '(preview-scale-function 1.2)
  '(prog-mode-hook '(flyspell-prog-mode))
