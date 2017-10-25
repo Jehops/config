948196b412ea64502625f9b1e0a9da323333b7fe
Joseph Mingrone <jrm@ftfl.ca>
2017-10-25 13:48:32 -0300

    Comment out uBlock code, since I'm not using that plugin

--- conkeror/003-extensions.js
+++ conkeror/003-extensions.js
@@ -35,18 +35,18 @@
 //     session_pref('extensions.mozrepl.initUrl', make_uri(mozrepl_init).spec);
 // }
 
-interactive(
-    "ublock", "Open uBlock dashboard in a new buffer",
-    function (I) {
-        var ublock_branch;
-        if ("@ublock0/content-policy;1" in Cc) {
-            ublock_branch = "ublock0";
-        } else if ("@ublock/content-policy;1" in Cc) {
-            ublock_branch = "ublock";
-        } else {
-            throw interactive_error("uBlock not found");
-        }
-        load_url_in_new_buffer("chrome://"+ublock_branch+"/content/dashboard.html");
-    }
-);
+// interactive(
+//     "ublock", "Open uBlock dashboard in a new buffer",
+//     function (I) {
+//         var ublock_branch;
+//         if ("@ublock0/content-policy;1" in Cc) {
+//             ublock_branch = "ublock0";
+//         } else if ("@ublock/content-policy;1" in Cc) {
+//             ublock_branch = "ublock";
+//         } else {
+//             throw interactive_error("uBlock not found");
+//         }
+//         load_url_in_new_buffer("chrome://"+ublock_branch+"/content/dashboard.html");
+//     }
+// );
 
