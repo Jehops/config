45de0fb1f4297b815cee9da4e2996c93df69542e
Joseph Mingrone <jrm@ftfl.ca>
2021-05-09 14:07:36 -0300

    custom.el: Use directory auto-saves/, not a auto_saves/

--- emacs/custom-lite.el
+++ emacs/custom-lite.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-file-name-transforms '((".*/\\([^/]*\\)" "~/.emacs.d/auto_saves/\\1" t)))
+ '(auto-save-file-name-transforms '((".*/\\([^/]*\\)" "~/.emacs.d/auto-saves/\\1" t)))
  '(backup-directory-alist '((".*" . "~/.emacs.d/.emacs_backups/")))
  '(before-save-hook '(time-stamp))
  '(blink-cursor-mode nil)
